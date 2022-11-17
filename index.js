7381f1b1c1545e65dc66f1f24731753020fb938d
matomugo60 <matomugo60@gmail.com>
2022-11-17 18:22:05 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-add(a+b);
-subtract(a-b);
-multiply(a*b);
-divide(a/b);
+function add() {}
+function subtract() {}
+function multiply () {}
+function divide() {}
 
-let number = 5;
-number +=
+function add(a,b) {
+    return a + b 
+}
+
+function subtract(a,b) {
+    return a - b
+}
+function multiply(a,b) {
+    return a * b
+}
+function divide(a,b) {
+    return a / b
+}
+
+function increment(n) {
+    return ++n
+}
+
+function decrement(n) {
+    return --n
+}
+
+function makeInt(n) {
+    return parseInt
+}
